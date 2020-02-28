4c855db2a81d1d41f54a9db7cc4e5da88a9e96be
Yo Yi <colinyoyo26@gmail.com>
2020-02-29 00:13:37 +0800

    Use to demonstrate xs_tok

---
+++ 2020q1-quiz2-xstring/test_output_tok.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "xs.h"
+
+int main(void)
+{
+    xs string;
+    char token[8] = "HELLO W", *tem;
+    xs_new(&string, "HELLOW fooHELLOWbarHbarfooLbarLbarObarW");
+    printf("#token\n%s\n", token);
+    printf("\n#initial string\n%s\n\n", xs_data(&string));
+
+    printf("%s\n", xs_tok(&string, token));
+    while(tem = xs_tok(NULL, token))
+        printf("%s\n", tem);
+    return 0;
+}
