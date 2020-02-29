bb6c1358f466a6ee12cb2e7e531ef5e2f20da0bf
Yo Yi <colinyoyo26@gmail.com>
2020-02-29 15:15:13 +0800

    Update for new implementation of CoW

--- 2020q1-quiz2-xstring/test_output_cow.c
+++ 2020q1-quiz2-xstring/test_output_cow.c
@@ -8,33 +8,31 @@ void print_info(char* str)
     printf("\n#string");
     printf("\nstring: %s", xs_data(&string));
     printf("\n&string: %p", xs_data(&string));
-    printf("\nreference count: %d\n", string.ref_cnt);
+    printf("\nreference count: %d\n", xs_refcnt(&string));
 
     printf("\n#cow1");
     printf("\nstring: %s", xs_data(&cow1));
     printf("\n&string: %p", xs_data(&cow1));
-    printf("\nreference count: %d\n", cow1.ref_cnt);
+    printf("\nreference count: %d\n", xs_refcnt(&cow1));
 
     printf("\n#cow2");
     printf("\nstring: %s", xs_data(&cow2));
     printf("\n&string: %p", xs_data(&cow2));
-    printf("\nreference count: %d\n", cow2.ref_cnt);
+    printf("\nreference count: %d\n", xs_refcnt(&cow2));
 }
 
 int main()
 {
     
     xs_new(&string, "gggfoobarbarbarbarbarzzz");
+    print_info("init");
     xs_cpy(&cow1, &string);
     xs_cpy(&cow2, &string);
     print_info("after cpy from string to cow1 & cow2");
     xs prefix = *xs_tmp("((("), suffix = *xs_tmp(")))");
-    xs_concat(&cow1, &prefix, &suffix);
-    print_info("after concat cow1");
-    xs_cpy(&cow2, &cow1);
-    print_info("after cpy from cow1 to cow2");
+    xs_concat(&string, &prefix, &suffix);
+    print_info("after concat string");
     xs_trim(&cow1, ")(zg");
     print_info("after trim cow1");
-
     return 0;
 }
