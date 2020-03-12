0eac0782894b20229af770ba59fa763a089cfa7d
colinyoyo26 <colinyoyo26@gmail.com>
2020-03-12 12:41:30 +0800

    Use `PLOT` macro to determine output format

--- 2020q1-quiz2-xstring/cpy_bench.c
+++ 2020q1-quiz2-xstring/cpy_bench.c
@@ -1,13 +1,13 @@
-#include "xs.h"
-#include <string.h>
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "xs.h"
 
 #define CPYS cpys
 int cpys;
 
-typedef enum {XSTR, CSTR} MODE;
+typedef enum { XSTR, CSTR } MODE;
 
 static inline double micro_time()
 {
@@ -47,26 +47,29 @@ int main(int argc, char **argv)
     int len = strlen(argv[1]);
     double time = 0;
 
-    if(mode == XSTR){
+    if (mode == XSTR) {
         xs x_in;
         xs_new(&x_in, argv[1]);
         xs x_str[CPYS];
         for (int i = 0; i < CPYS; i++)
             xs_new(&x_str[i], "");
         time = test_x_str(x_str, x_in);
+#ifndef PLOT
         printf("MODE: XSTR\n");
         printf("refcnt: %ld\n", xs_refcnt(&x_in));
-    }
-    else{
+#endif
+    } else {
         char *c_in = argv[1];
         char *c_str[CPYS];
         for (int i = 0; i < CPYS; i++)
             c_str[i] = malloc((len + 1) * sizeof(char));
         time = test_c_str(c_str, c_in);
+#ifndef PLOT
         printf("MODE: CSTR\n");
+#endif
     }
 
-    
+
     printf("time: %lf\n", time);
     return 0;
 }
